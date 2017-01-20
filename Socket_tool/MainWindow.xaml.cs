0172c0ae9b0eafe00a71bdd9d5c6128361d58280
caozeyu <caozy@foxmail.com>
2017-01-20 16:46:31 +0800

    初步的界面设计

--- Socket_tool/MainWindow.xaml.cs
+++ Socket_tool/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MaterialDesignThemes;
+using MaterialDesignThemes.Wpf;
 
 namespace Socket_tool
 {
@@ -27,90 +30,104 @@ namespace Socket_tool
         {
             InitializeComponent();
         }
-        private void port_text_KeyDown(object sender, KeyEventArgs e)
-        {
-            TextBox txt = sender as TextBox;
 
-            //屏蔽非法按键
-            if ((e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || e.Key == Key.Decimal || e.Key.ToString() == "Tab")
-            {
-                if (e.Key == Key.Decimal)
-                {
-                    e.Handled = true;
-                    return;
-                }
-                e.Handled = false;
-            }
-            else if (((e.Key >= Key.D0 && e.Key <= Key.D9) || e.Key == Key.OemPeriod) && e.KeyboardDevice.Modifiers != ModifierKeys.Shift)
-            {
-                if (e.Key == Key.OemPeriod)
-                {
-                    e.Handled = true;
-                    return;
-                }
-                e.Handled = false;
-            }
-            else
-            {
-                e.Handled = true;
-            }
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            DataExchange data = new DataExchange();
+            data_exchange.Children.Add(data);
+            
         }
 
-        private void port_text_TextChanged(object sender, TextChangedEventArgs e)
+        private void button1_Click(object sender, RoutedEventArgs e)
         {
-            TextBox textBox = sender as TextBox;
-            TextChange[] change = new TextChange[e.Changes.Count];
-            e.Changes.CopyTo(change, 0);
+            string hei = client_grid.ActualHeight.ToString();
+            string wei = client_grid.ActualWidth.ToString();
+            MessageBox.Show(hei + " " + wei);
+        }
+        //private void port_text_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    TextBox txt = sender as TextBox;
 
-            int offset = change[0].Offset;
-            if (change[0].AddedLength > 0)
-            {
-                double num = 0;
-                if (!Double.TryParse(textBox.Text, out num))
-                {
-                    textBox.Text = textBox.Text.Remove(offset, change[0].AddedLength);
-                    textBox.Select(offset, 0);
-                }
-            }
-        }
+        //    //屏蔽非法按键
+        //    if ((e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || e.Key == Key.Decimal || e.Key.ToString() == "Tab")
+        //    {
+        //        if (e.Key == Key.Decimal)
+        //        {
+        //            e.Handled = true;
+        //            return;
+        //        }
+        //        e.Handled = false;
+        //    }
+        //    else if (((e.Key >= Key.D0 && e.Key <= Key.D9) || e.Key == Key.OemPeriod) && e.KeyboardDevice.Modifiers != ModifierKeys.Shift)
+        //    {
+        //        if (e.Key == Key.OemPeriod)
+        //        {
+        //            e.Handled = true;
+        //            return;
+        //        }
+        //        e.Handled = false;
+        //    }
+        //    else
+        //    {
+        //        e.Handled = true;
+        //    }
+        //}
 
-        private void start_server_Click(object sender, RoutedEventArgs e)
-        {
-            int port = 0;
-            if (int.TryParse(port_text.Text, out port))
-            {
-                if (port == 0 || port > 65535)
-                {
-                    MessageBox.Show("错误", "端口数值错误!");
-                }
-                IPAddress ip = IPAddress.Parse("0.0.0.0");
-                ser = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                ser.Bind(new IPEndPoint(ip, port));
-                ser.Listen(10);
-                Thread myThread = new Thread(listen_client);
-                myThread.Start();
-            }
-        }
-        private void listen_client()
-        {
-            byte[] res = new byte[1024];
-            while (true)
-            {
-                Socket client = ser.Accept();
-                this.Dispatcher.Invoke(new Action(() => {
-                    client_list.Items.Add(client.RemoteEndPoint.ToString());
-                }));
-                int len = client.Receive(res);
-                this.Dispatcher.Invoke(new Action(() => {
-                    //recv_block.Text = Encoding.ASCII.GetString(res, 0, len);
-                }));
-            }
-        }
+        //private void port_text_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    TextBox textBox = sender as TextBox;
+        //    TextChange[] change = new TextChange[e.Changes.Count];
+        //    e.Changes.CopyTo(change, 0);
 
-        private void disconnect_Click(object sender, RoutedEventArgs e)
-        {
-            ser.Close();
-        }
+        //    int offset = change[0].Offset;
+        //    if (change[0].AddedLength > 0)
+        //    {
+        //        double num = 0;
+        //        if (!Double.TryParse(textBox.Text, out num))
+        //        {
+        //            textBox.Text = textBox.Text.Remove(offset, change[0].AddedLength);
+        //            textBox.Select(offset, 0);
+        //        }
+        //    }
+        //}
+
+        //private void start_server_Click(object sender, RoutedEventArgs e)
+        //{
+        //    int port = 0;
+        //    if (int.TryParse(port_text.Text, out port))
+        //    {
+        //        if (port == 0 || port > 65535)
+        //        {
+        //            MessageBox.Show("错误", "端口数值错误!");
+        //        }
+        //        IPAddress ip = IPAddress.Parse("0.0.0.0");
+        //        ser = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //        ser.Bind(new IPEndPoint(ip, port));
+        //        ser.Listen(10);
+        //        Thread myThread = new Thread(listen_client);
+        //        myThread.Start();
+        //    }
+        //}
+        //private void listen_client()
+        //{
+        //    byte[] res = new byte[1024];
+        //    while (true)
+        //    {
+        //        Socket client = ser.Accept();
+        //        this.Dispatcher.Invoke(new Action(() => {
+        //            client_list.Items.Add(client.RemoteEndPoint.ToString());
+        //        }));
+        //        int len = client.Receive(res);
+        //        this.Dispatcher.Invoke(new Action(() => {
+        //            //recv_block.Text = Encoding.ASCII.GetString(res, 0, len);
+        //        }));
+        //    }
+        //}
+
+        //private void disconnect_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ser.Close();
+        //}
     }
    
 }
